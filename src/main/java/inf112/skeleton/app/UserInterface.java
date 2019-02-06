8088e57145931b300c9a32d7bf5d10562b564cdc
riv009 <stolte27gmail.com>
2019-02-06 21:16:28 +0100

    Added texture to the assets file. Added UserInterface, which draws board with players

--- src/main/java/inf112/skeleton/app/UserInterface.java
+++ src/main/java/inf112/skeleton/app/UserInterface.java
@@ -1,15 +1,28 @@
 package inf112.skeleton.app;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class UserInterface implements ApplicationListener {
 
+    private Texture normalFloorTexture;
+    private int normalFloorSpriteSize;
+    private Texture robotTexture;
+    
     private SpriteBatch batch;
+    private IBoard board;
+    
+    public UserInterface(IBoard board) {
+        this.board = board;
+    }
     
     @Override
     public void create() {
         batch = new SpriteBatch();
+        normalFloorTexture = new Texture(Gdx.files.internal("assets/StoneTile"));
+        robotTexture = new Texture(Gdx.files.internal("assets/GreenRobot"));
     }
 
     @Override
@@ -21,6 +34,16 @@ public class UserInterface implements ApplicationListener {
     @Override
     public void render() {
         batch.begin();
+        for(int i = 0; i < board.getWidth(); i++) {
+            for(int j = 0; j < board.getHeight(); j++) {
+                // Draws a normal tile on each square of the grid
+                batch.draw(normalFloorTexture, normalFloorSpriteSize * i, normalFloorSpriteSize * j);
+                // If there is a player on the tile, draws a player ontop of it
+                if(board.getTile(i, j) == 1) {
+                    batch.draw(robotTexture, normalFloorSpriteSize * i, normalFloorSpriteSize * j);
+                }
+            }
+        }
         batch.end();
     }
 
