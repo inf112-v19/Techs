71e0121beee251e2eb15f5449e4f2a02b977cc14
Knut Storvestre <huq006@uib.no>
2019-03-03 19:25:10 +0100

    rename stuff

--- src/main/java/inf112/skeleton/app/ProgramSheet.java
+++ src/main/java/inf112/skeleton/app/ProgramSheet.java
@@ -1,4 +1,61 @@
 package inf112.skeleton.app;
 
-public class ProgramSheet {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class ProgramSheet implements Screen {
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
+    @Override
+    public void show() {
+        //map = new TmxMapLoader().load("assets/RoboRallyMap.tmx");
+        map = new TmxMapLoader().load("assets/ProgramSheet/ProgramSheet.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, .3f);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.setView(camera);
+        renderer.render();
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
