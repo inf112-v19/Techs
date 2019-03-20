8582107a50ef33522b6a56db6a02cff2d2a483f1
Knut Storvestre <huq006@uib.no>
2019-03-20 13:57:00 +0100

    another one

--- src/main/java/inf112/skeleton/app/GameController.java
+++ src/main/java/inf112/skeleton/app/GameController.java
@@ -1,24 +1,41 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Screen;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Game {
-
+public class GameController {
 
     private ArrayList<ArrayList<IProgramCard>> programCards;
     //player 1 har pos 0 og så videre
     private ArrayList<PlayerToken> playerTokens;
     private int numPlayers;
-    //private int playerTurn;
-    BoardCards boardCards = new BoardCards();
+    private RoboRally roboRally;
+    private BoardCards boardCards;
 
-    public Game(int numPlayers){
+    private PlayerToken player1;
+    private PlayerToken player2;
+
+    private HashMap<Integer, ArrayList<IProgramCard>> playersCards;
+
+    public GameController(int numPlayers, RoboRally roboRally){
         this.numPlayers = numPlayers;
+        this.roboRally = roboRally;
+        boardCards = new BoardCards(roboRally);
         turn(0);
+    }
+
+    public void createNewPlayer(){
+        //boardCards.createNewPlayer();
     }
 
     public void turn(int playerTurn){
 
+    }
+
+    public Screen getScreen(){
+        return boardCards;
     }
 
     //private
