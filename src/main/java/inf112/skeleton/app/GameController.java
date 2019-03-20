f5c3418d2db407c3f65865cbbdd4f7ec77318790
Knut Storvestre <huq006@uib.no>
2019-03-20 13:56:35 +0100

    Board endret til å lage GameController

--- src/main/java/inf112/skeleton/app/GameController.java
+++ src/main/java/inf112/skeleton/app/GameController.java
@@ -6,12 +6,19 @@ public class Game {
 
 
     private ArrayList<ArrayList<IProgramCard>> programCards;
+    //player 1 har pos 0 og så videre
     private ArrayList<PlayerToken> playerTokens;
     private int numPlayers;
-    private int playerTurn;
+    //private int playerTurn;
+    BoardCards boardCards = new BoardCards();
 
     public Game(int numPlayers){
         this.numPlayers = numPlayers;
+        turn(0);
+    }
+
+    public void turn(int playerTurn){
+
     }
 
     //private
