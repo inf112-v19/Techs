62c632c82d06628883415fb78e1c6d5da035c195
Knut Storvestre <huq006@uib.no>
2019-03-26 14:27:37 +0100

    forbedringer i IGameController interface

--- src/main/java/inf112/skeleton/app/GameController.java
+++ src/main/java/inf112/skeleton/app/GameController.java
@@ -7,7 +7,7 @@ import inf112.skeleton.app.objects.PlayerToken;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class GameController {
+public class GameController implements IGameController{
     private ArrayList<ArrayList<IProgramCard>> programCards;
     //player 1 har pos 0 og så videre
     private ArrayList<PlayerToken> playerTokens;
@@ -21,38 +21,48 @@ public class GameController {
         playersCards = new HashMap<>();
     }
 
+    @Override
     public void donePickingCards(ArrayList<IProgramCard> cardsCurrentPlayer, BoardCards boardCards){
         int currentPlayer = turns % numPlayers;
         playersCards.put(currentPlayer, cardsCurrentPlayer);
         turns++;
-        turn(turns % numPlayers, boardCards);
+        if (turns % numPlayers == 0)
+            movePlayers(boardCards);
     }
 
-    public void turn(int playerTurn, BoardCards boardCards){
-        if (playerTurn == 0){
-            ArrayList<IProgramCard> cards1 = playersCards.get(0);
-            ArrayList<IProgramCard> cards2 = playersCards.get(1);
+    @Override
+    public void movePlayers(BoardCards boardCards){
+        ArrayList<IProgramCard> cards1 = playersCards.get(0);
+        ArrayList<IProgramCard> cards2 = playersCards.get(1);
 
-            for (int i = 0; i < 5; i++) {
-                if (cards1.get(0).getPriority() > cards2.get(0).getPriority()) {
-                    doTurn(cards1.remove(0), 0, boardCards);
-                    doTurn(cards2.remove(0), 1, boardCards);
-                }
-                else {
-                    doTurn(cards2.remove(0), 1, boardCards);
-                    doTurn(cards1.remove(0), 0, boardCards);
-                }
+        for (int i = 0; i < 5; i++) {
+            if (cards1.get(0).getPriority() > cards2.get(0).getPriority()) {
+                movePlayer(cards1.remove(0), 0, boardCards);
+                movePlayer(cards2.remove(0), 1, boardCards);
+            }
+            else {
+                movePlayer(cards2.remove(0), 1, boardCards);
+                movePlayer(cards1.remove(0), 0, boardCards);
             }
         }
     }
-    
-    private void doTurn(IProgramCard programCard, int currentPlayer, BoardCards boardCards) {
+
+    @Override
+    public void movePlayer(IProgramCard programCard, int currentPlayer, BoardCards boardCards) {
         if (programCard.getDirection() != 0) {
             boardCards.getBoardLogic().getPlayersList().get(currentPlayer).rotatePlayer(programCard.getDirection());
         }
         else
             for (int i = 0; i < programCard.getMovement(); i++)
                 boardCards.getBoardLogic().getPlayersList().get(currentPlayer).moveInFacingDirection();
+    }
+
+    @Override
+    public ArrayList<IProgramCard> getProgramCardToPlayer(int player) {
+        if (player >= playersCards.keySet().size() || player < 0){
+            throw new IllegalArgumentException("Player does not exist");
+        }
+        return playersCards.get(player);
     }
 
 }
--- src/main/java/inf112/skeleton/app/IGameController.java
+++ src/main/java/inf112/skeleton/app/IGameController.java
@@ -11,19 +11,31 @@ public interface IGameController {
      * The use of this method is intended for BoardCards
      * When a player is done picking cards BoardCards send the cards to GameController that saves them
      * @param cardsCurrentPlayer ProgramCards to player
-     * @param boardCards The screen for the board
+     * @param boardCards The screen for the board and cards
      */
     void donePickingCards(ArrayList<IProgramCard> cardsCurrentPlayer, BoardCards boardCards);
 
     /**
-     *
-     * @param playerTurn
-     * @param boardCards
+     *This method pics the card with highest priority and sends it to movePlayer
+     * @param boardCards The screen for the board and cards
      */
-    void turn(int playerTurn, BoardCards boardCards);
+    void movePlayers(BoardCards boardCards);
 
-    void doTurn(IProgramCard programCard, int currentPlayer, BoardCards boardCards);
+    /**
+     * This method takes the information from programCard and
+     * tells BoardLogic where to move the player.
+     * @param programCard
+     * @param currentPlayer the owner of the programCard
+     * @param boardCards Screen
+     */
+    void movePlayer(IProgramCard programCard, int currentPlayer, BoardCards boardCards);
 
+    /**
+     * Intended for testing
+     * @param player 0 to (numberOf players in the game - 1).
+     *               0 is starting player. 1 is player after starting player and so on.
+     * @return programCards to to the player
+     */
     ArrayList<IProgramCard> getProgramCardToPlayer(int player);
 
 }
