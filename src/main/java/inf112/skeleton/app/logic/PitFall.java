970829a576115ae590269f946c9b379a4a3cc471
mlmadslien <gis009@uib.no>
2019-03-27 13:40:39 +0100

    PitFall klasse

---
+++ src/main/java/inf112/skeleton/app/logic/PitFall.java
@@ -0,0 +1,37 @@
+package inf112.skeleton.app.logic;
+
+import java.util.ArrayList;
+
+import inf112.skeleton.app.objects.PlayerToken;
+
+public class PitFall implements IBoardFeature {
+
+	private BoardLogic boardLogic;
+	private ArrayList<PlayerToken> playersList;
+    private ArrayList<String> playersChecked;
+    private String layerName = "Pit";
+	
+	public PitFall(BoardLogic boardLogic, ArrayList<PlayerToken> playersList) {
+		this.playersList = playersList;
+		this.boardLogic = boardLogic;		
+	}
+
+	@Override
+	public void processFeature() {
+			
+	}
+	
+	public void movePlayerIfPitFall(PlayerToken player) {
+		if(playersChecked.contains(player.getName()))
+			return;
+		
+		playersChecked.add(player.getName());
+        int xPos = player.getXPosition();
+        int yPos = player.getYPosition();
+        
+        if(boardLogic.cellContainsLayer(xPos, yPos, layerName)) {
+        	
+        }
+	}
+
+}
