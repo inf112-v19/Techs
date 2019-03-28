ac91fbed561b35deaf6d63c1f3d52e848d653241
RenzeStolte <stolte27@gmail.com>
2019-03-28 15:40:18 +0100

    Added conveyorbelt tests

--- src/test/java/inf112/skeleton/app/MoveConveyerBeltsTests.java
+++ src/test/java/inf112/skeleton/app/MoveConveyerBeltsTests.java
@@ -2,9 +2,19 @@ package inf112.skeleton.app;
 
 import static org.junit.Assert.assertEquals;
 
-import inf112.skeleton.app.logic.BoardLogic;
-import org.junit.Test;
+import org.junit.Before;
 
+import inf112.skeleton.app.logic.BoardLogic;
+import inf112.skeleton.app.logic.Direction;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -13,28 +23,76 @@ import com.badlogic.gdx.math.Vector2;
 
 public class MoveConveyerBeltsTests {
     
-    private BoardLogic boardLogic;
-    private Sprite robotSprite;
-    private TiledMap map;    
+    private static Application application;
+
+    private BoardLogic circleBoard;    
 
     @Before
     public void setUp() {
-        robotSprite = new Sprite (new Texture("assets/GreenRobot.png"));
-        map = new TmxMapLoader().load("assets/RoboRallyMap.tmx");
-        boardLogic = new BoardLogic(map);
+        application = new HeadlessApplication(new ApplicationListener() {
+            @Override public void create() {}
+            @Override public void resize(int width, int height) {}
+            @Override public void render() {}
+            @Override public void pause() {}
+            @Override public void resume() {}
+            @Override public void dispose() {}
+        });
+        
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+        
+        TiledMap circleMap = new TmxMapLoader().load("assets/testMaps/mapCircleConveyor.tmx");
+        circleBoard = new BoardLogic(circleMap);
     }
     
     @Test
-    public void testPlayerMovesToTheRight() {
-        robotSprite = null;
-        map = new TmxMapLoader().load("assets/RoboRallyMap.tmx");
-        boardLogic = new BoardLogic(map);
-
-        
-        boardLogic.addPlayerToBoard(new Vector2(0,1), "testPlayer");
-        
-
-        boardLogic.moveConveyorBelts();
-        assertEquals(new Vector2(0,2), boardLogic.getPlayerLocation("testPlayer"));
+    public void playerMovesLeft() {
+        circleBoard.addPlayerToBoard(new Vector2(1, 2), "testPlayerOne");
+        circleBoard.moveConveyorBelts();
+        assertEquals(new Vector2(0, 2), circleBoard.getPlayerLocation("testPlayerOne"));
+    }
+    
+    @Test
+    public void playerMovesRight() {
+        circleBoard.addPlayerToBoard(new Vector2(1, 0), "testPlayerOne");
+        circleBoard.moveConveyorBelts();
+        assertEquals(new Vector2(2, 0), circleBoard.getPlayerLocation("testPlayerOne"));
+    }
+    @Test
+    public void playerMovesDown() {
+        circleBoard.addPlayerToBoard(new Vector2(0, 1), "testPlayerOne");
+        circleBoard.moveConveyorBelts();
+        assertEquals(new Vector2(0, 0), circleBoard.getPlayerLocation("testPlayerOne"));
+    }
+    @Test
+    public void playerMovesUp() {
+        circleBoard.addPlayerToBoard(new Vector2(2, 1), "testPlayerOne");
+        circleBoard.moveConveyorBelts();
+        assertEquals(new Vector2(2, 2), circleBoard.getPlayerLocation("testPlayerOne"));
+    }
+    @Test
+    public void playerRotates() {
+        circleBoard.addPlayerToBoard(new Vector2(1, 2), "testPlayerOne");
+        circleBoard.moveConveyorBelts();
+        assertEquals(Direction.WEST, circleBoard.getPlayerRotation("testPlayerOne"));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
