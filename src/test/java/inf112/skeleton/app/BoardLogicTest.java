48c64d4b2180c7dac9e4f6ae64a2c84d28738f8c
RenzeStolte <stolte27@gmail.com>
2019-03-28 11:46:44 +0100

    Clean testing enabled, using Headless app and mockito

--- src/test/java/inf112/skeleton/app/BoardLogicTest.java
+++ src/test/java/inf112/skeleton/app/BoardLogicTest.java
@@ -1,163 +1,110 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.loaders.resolvers.ExternalFileHandleResolver;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
+
+import inf112.skeleton.app.logic.BoardCards;
 import inf112.skeleton.app.logic.BoardLogic;
 import inf112.skeleton.app.logic.Direction;
 import inf112.skeleton.app.screens.Board;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static junit.framework.TestCase.assertEquals;
 
 
 public class BoardLogicTest {
-	
-    private boolean waitForThread;
-    private boolean testcase;
+    
+    private static Application application;
+    private TiledMap noWallMap;
+    private TiledMap wallMap;
+    private BoardLogic noWallBoard;
+    private BoardLogic wallBoard;
 
+    
     @Before
-    public void setUp() {
-    	// Nothing in setup works
-    	
-    	/*
-    	noWallMap = new TmxMapLoader().load("assets/testMaps/mapNoWalls.tmx");
+    public void setUp() {    
+        application = new HeadlessApplication(new ApplicationListener() {
+            @Override public void create() {}
+            @Override public void resize(int width, int height) {}
+            @Override public void render() {}
+            @Override public void pause() {}
+            @Override public void resume() {}
+            @Override public void dispose() {}
+        });
+        
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+        
+        noWallMap = new TmxMapLoader().load("assets/testMaps/mapNoWalls.tmx");
         wallMap = new TmxMapLoader().load("assets/testMaps/mapAllWalls.tmx");
-        board.addPlayerToBoard(new Vector2(1,1), "PlayerOne");
-        */
-    	
-    	/*
-    	// This didn't work for some reason, app stays null
-    	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.width = 480;
-        cfg.height = 320;
-        cfg.resizable = true;
-        this.app = new LwjglApplication( new RoboRally(), cfg);
-        */
+        
+        noWallBoard = new BoardLogic(noWallMap);
+        wallBoard = new BoardLogic(wallMap);
+    }
+
+    @Test
+    public void addPlayerToBoard() {
+        noWallBoard.addPlayerToBoard(new Vector2(0,0), "newPlayer");
+        assertEquals(new Vector2(0,0), noWallBoard.getPlayerLocation("newPlayer"));
     }
     
     @Test
-    public void testingTest() {
-    	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.width = 480;
-        cfg.height = 320;
-        cfg.resizable = true;
-        LwjglApplication app = new LwjglApplication( new RoboRally(), cfg);
-    	waitForThread = true;
-    	testcase = false;
-        
-    	Gdx.app.postRunnable(new Runnable() {
-			@Override
-			public void run() {
-            	TiledMap noWallMap = new TmxMapLoader().load("assets/testMaps/mapNoWalls.tmx");
-            	BoardLogic board = new BoardLogic(noWallMap);
-            	board.addPlayerToBoard(new Vector2(0,0), "playerOne");
-            	assertEquals(new Vector2(0,0), board.getPlayerLocation("playerOne"));
-            	if((new Vector2(0,0).equals(board.getPlayerLocation("playerOne"))))
-            			testcase = true;
-            	waitForThread = false;
-			}
-    	});
-    	
-    	while(waitForThread) {
-            try {
-                Thread.sleep(10);
-            } catch(Exception e ) {
-            }
-        }
-    	assertTrue(testcase);
-    }
-/*
-    @Test
     public void movePlayerTest() {
-    	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.width = 480;
-        cfg.height = 320;
-        cfg.resizable = true;
-        LwjglApplication app = new LwjglApplication( new RoboRally(), cfg);
-    	app.postRunnable(new Runnable() {
-			@Override
-			public void run() {
-            	TiledMap noWallMap = new TmxMapLoader().load("assets/testMaps/mapNoWalls.tmx");
-            	BoardLogic board = new BoardLogic(noWallMap);
-            	board = new BoardLogic(noWallMap);
-                assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,1));
-                board.movePlayer("PlayerOne", Direction.NORTH);
-                assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,2));
-                board.movePlayer("PlayerOne", Direction.WEST);
-                assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(0,2));
-                board.movePlayer("PlayerOne", Direction.SOUTH);
-                assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(0,1));
-                board.movePlayer("PlayerOne", Direction.EAST);
-                assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,1));
-			}
-    	});
+        noWallBoard.addPlayerToBoard(new Vector2(1,1), "PlayerOne");
+        assertEquals(new Vector2(1,1), noWallBoard.getPlayerLocation("PlayerOne"));
+        noWallBoard.movePlayer("PlayerOne", Direction.NORTH);
+        assertEquals(new Vector2(1,2), noWallBoard.getPlayerLocation("PlayerOne"));
+        noWallBoard.movePlayer("PlayerOne", Direction.WEST);
+        assertEquals(new Vector2(0,2), noWallBoard.getPlayerLocation("PlayerOne"));
+        noWallBoard.movePlayer("PlayerOne", Direction.SOUTH);
+        assertEquals(new Vector2(0,1), noWallBoard.getPlayerLocation("PlayerOne"));
+        noWallBoard.movePlayer("PlayerOne", Direction.EAST);        
+        assertEquals(new Vector2(1,1), noWallBoard.getPlayerLocation("PlayerOne"));
     }
 
     @Test
     public void wallCollisionTest() {
-    	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.width = 480;
-        cfg.height = 320;
-        cfg.resizable = true;
-        LwjglApplication app = new LwjglApplication( new RoboRally(), cfg);
-    	app.postRunnable(new Runnable() {
-			@Override
-			public void run() {
-				board = new BoardLogic(wallMap);
-				assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,3));
-				board.movePlayer("PlayerOne", Direction.NORTH);
-				assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,1));
-				board.movePlayer("PlayerOne", Direction.WEST);
-				assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,1));
-				board.movePlayer("PlayerOne", Direction.SOUTH);
-				assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,1));
-				board.movePlayer("PlayerOne", Direction.EAST);
-				assertEquals(board.getPlayerLocation("PlayerOne"), new Vector2(1,1));
-			}
-    	});
+        wallBoard.addPlayerToBoard(new Vector2(1,1), "PlayerOne");
+		assertEquals(new Vector2(1,1), wallBoard.getPlayerLocation("PlayerOne"));
+		wallBoard.movePlayer("PlayerOne", Direction.NORTH);
+		assertEquals(new Vector2(1,1), wallBoard.getPlayerLocation("PlayerOne"));
+		wallBoard.movePlayer("PlayerOne", Direction.WEST);
+		assertEquals(new Vector2(1,1), wallBoard.getPlayerLocation("PlayerOne"));
+		wallBoard.movePlayer("PlayerOne", Direction.SOUTH);
+		assertEquals(new Vector2(1,1), wallBoard.getPlayerLocation("PlayerOne"));
+		wallBoard.movePlayer("PlayerOne", Direction.EAST);
+		assertEquals(new Vector2(1,1), wallBoard.getPlayerLocation("PlayerOne"));
     }
 
     @Test
     public void playerRotationTest() {
-    	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.width = 480;
-        cfg.height = 320;
-        cfg.resizable = true;
-        LwjglApplication app = new LwjglApplication( new RoboRally(), cfg);
-    	app.postRunnable(new Runnable() {
-			@Override
-			public void run() {
-				
-				board = new BoardLogic(noWallMap);
-				assertEquals(Direction.NORTH, board.getPlayerRotation("PlayerOne"));
-				board.rotatePlayer("PlayerOne", 1);
-				assertEquals(Direction.EAST, board.getPlayerRotation("PlayerOne"));
-				board.rotatePlayer("PlayerOne", 1);
-				assertEquals(Direction.SOUTH, board.getPlayerRotation("PlayerOne"));
-				board.rotatePlayer("PlayerOne", 1);
-				assertEquals(Direction.WEST, board.getPlayerRotation("PlayerOne"));
-				board.rotatePlayer("PlayerOne", 1);
-				assertEquals(Direction.NORTH, board.getPlayerRotation("PlayerOne"));
-			}
-    	});
+        noWallBoard.addPlayerToBoard(new Vector2(1,1), "PlayerOne");
+		assertEquals(Direction.NORTH, noWallBoard.getPlayerRotation("PlayerOne"));
+		noWallBoard.rotatePlayer("PlayerOne", 1);
+		assertEquals(Direction.EAST, noWallBoard.getPlayerRotation("PlayerOne"));
+		noWallBoard.rotatePlayer("PlayerOne", 1);
+		assertEquals(Direction.SOUTH, noWallBoard.getPlayerRotation("PlayerOne"));
+		noWallBoard.rotatePlayer("PlayerOne", 1);
+		assertEquals(Direction.WEST, noWallBoard.getPlayerRotation("PlayerOne"));
+		noWallBoard.rotatePlayer("PlayerOne", 1);
+		assertEquals(Direction.NORTH, noWallBoard.getPlayerRotation("PlayerOne"));
     }
-    
-    /*
-    @After
-    public void disposeOfMap() {
-        wallMap.dispose();
-        noWallMap.dispose();
-        // Probably not needed
-        // board.testEnder();
-    }*/
 }
