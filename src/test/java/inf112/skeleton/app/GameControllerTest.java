370fecfef127f1369e1216e7c528562137e67935
Knut Storvestre <huq006@uib.no>
2019-03-29 14:06:42 +0100

    manuelle tester for BoardCards

--- src/test/java/inf112/skeleton/app/GameControllerTest.java
+++ src/test/java/inf112/skeleton/app/GameControllerTest.java
@@ -3,25 +3,61 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.logic.BoardCards;
 import inf112.skeleton.app.logic.BoardLogic;
 
+import inf112.skeleton.app.logic.CardType;
+import inf112.skeleton.app.objects.IProgramCard;
+import inf112.skeleton.app.objects.ProgramCard;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class GameControllerTest {
 
     private TiledMap noWallMap;
-    private TiledMap wallMap;
-    private BoardLogic board;
+    //private TiledMap wallMap;
+    private BoardLogic boardLogic;
 
     @Before
     public void setUp(){
         noWallMap = new TmxMapLoader().load("assets/testMaps/mapNoWalls.tmx");
-        wallMap = new TmxMapLoader().load("assets/testMaps/mapAllWalls.tmx");
-        //GameController gameController = new GameController(2);
+        //wallMap = new TmxMapLoader().load("assets/testMaps/mapAllWalls.tmx");
+        boardLogic = new BoardLogic(noWallMap);
+        RoboRally roboRally = new RoboRally();
+        BoardCards boardCards = new BoardCards(roboRally, 1);
+        GameController gameController = new GameController(1, boardCards);
     }
 
+    @Test
+    void donePickingCards() {
+        ArrayList<IProgramCard> programCards = new ArrayList<>();
+        programCards.add(new ProgramCard(CardType.MOVEMENT_1, 1, 0, 1));
+        programCards.add(new ProgramCard(CardType.MOVEMENT_1, 1, 0, -1));
+        programCards.add(new ProgramCard(CardType.MOVEMENT_1, 1, 0, 1));
+        programCards.add(new ProgramCard(CardType.MOVEMENT_1, 1, 0, -1));
+        programCards.add(new ProgramCard(CardType.MOVEMENT_1, 1, 0, 1));
 
+    }
 
+    @Test
+    void movePlayers() {
 
+    }
+
+    @Test
+    void movePlayer() {
+    }
+
+    @Test
+    void getProgramCardToPlayer() {
+    }
+
+    @Test
+    void getTurns() {
+
+    }
 }
---
+++ src/test/java/inf112/skeleton/app/ManuelleTester/BoardCardsTest.md
@@ -0,0 +1,21 @@
+#Test BoardCards
+## Tall tester
+    * Test tall
+        * Når du trykker tallene 1-9 på tastaturet vil tallene i nederste 
+        hjørne flytte seg oppå kortet du har plukket.
+    
+    * Test tall til ny tur
+        * Sjekk at etter du har plukket ut 5 kort og trykket enter at tallene
+        flytter seg tilbake til nederste høyre hjørne.
+#
+##Kort Tester
+    * Test kort tilfeldighet
+        * Når du har plukket ut 5 kort og trykket enter vil det komme fram 
+        5 nye tilfeldige kort.
+        
+    * Test at spilleren flytter seg etter det kortet beskriver.
+    
+    * Test kort rekkefølge
+        * Se at spilleren flytter seg i rekkefølgen etter kortene som har blitt plukket ut.
+    
+#
