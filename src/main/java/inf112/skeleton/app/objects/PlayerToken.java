d9236e9331a6530bd5284ff040cf446f4d96b778
mlmadslien <gis009@uib.no>
2019-04-03 13:55:19 +0200

    Health and DamageTokens

--- src/main/java/inf112/skeleton/app/objects/PlayerToken.java
+++ src/main/java/inf112/skeleton/app/objects/PlayerToken.java
@@ -8,282 +8,296 @@ import inf112.skeleton.app.logic.Direction;
 
 public class PlayerToken extends Sprite {
 
-    private static final float ROTATE_SPEED = 400;
-    private static final float MOVEMENT_SPEED = 360;
-    private static final float ANIMATION_SPEED = 0.06f;
-    private static final int FRAME_COL = 8;
-    private static final int FRAME_ROW = 2;
-    private static final float TILE_SCALE = 96;
-    // Variables needed for movement, direction and position
-    private Vector2 movementVelocity = new Vector2();
-    private Vector2 backupPosition;
-    private Vector2 position;
-    private Direction facingDirection;
-    private boolean movingNorth = true;
-    private boolean movingSouth = true;
-    private boolean movingEast = true;
-    private boolean movingWest = true;
-    
-    private boolean rotatingLeft;
-    private boolean rotatingRight;
-    private int targetRotation;
-    private boolean recentlyBackuped;
-    
-    private int damageToken = 0;
+	private static final float ROTATE_SPEED = 400;
+	private static final float MOVEMENT_SPEED = 360;
+	private static final float ANIMATION_SPEED = 0.06f;
+	private static final int FRAME_COL = 8;
+	private static final int FRAME_ROW = 2;
+	private static final float TILE_SCALE = 96;
+	// Variables needed for movement, direction and position
+	private Vector2 movementVelocity = new Vector2();
+	private Vector2 backupPosition;
+	private Vector2 position;
+	private Direction facingDirection;
+	private boolean movingNorth = true;
+	private boolean movingSouth = true;
+	private boolean movingEast = true;
+	private boolean movingWest = true;
+
+	private boolean rotatingLeft;
+	private boolean rotatingRight;
+	private int targetRotation;
+	private boolean recentlyBackuped;
+
+	private int damageToken = 0;
 	private int health = 3;
 	private boolean destroyed = false;
 	private Vector2 archiveMarker;
 
-    private int numberOfCheckpointsPassed;
-    
-    // Variables needed for animated sprites
-    private String playerName;
-    private TextureRegion[] animationFrames;
-    private Animation<TextureRegion> robotAnimation;
-    private Texture spriteSheet;
-   
-    public PlayerToken(String givenName, String textureSpriteSheet, Vector2 startPosition) {
-        this.playerName = givenName;
-        this.archiveMarker = startPosition;
-        backupPosition = new Vector2(startPosition.x, startPosition.y);
-        position = startPosition;
-        facingDirection = Direction.NORTH;
-        recentlyBackuped = true;
+	private int numberOfCheckpointsPassed;
 
-        // All regarding spritesheet and getting the frames correctly is done here.
-        spriteSheet = new Texture(textureSpriteSheet);
-        TextureRegion[][] tmp = TextureRegion.split(spriteSheet, spriteSheet.getWidth() / FRAME_COL, spriteSheet.getHeight() / FRAME_ROW);
-        animationFrames = new TextureRegion[(FRAME_ROW * FRAME_COL) - 5];
-        int index = 0;
-        for (int i = 0; i < FRAME_ROW; i++) {
-            for (int j = 0; j < FRAME_COL; j++) {
-                if (i == 1 && j >= 3) {
-                    continue;
-                }
-                animationFrames[index++] = tmp[i][j];
-            }
-        }
+	// Variables needed for animated sprites
+	private String playerName;
+	private TextureRegion[] animationFrames;
+	private Animation<TextureRegion> robotAnimation;
+	private Texture spriteSheet;
 
-        robotAnimation = new Animation<TextureRegion>(ANIMATION_SPEED, animationFrames);
+	public PlayerToken(String givenName, String textureSpriteSheet, Vector2 startPosition) {
+		this.playerName = givenName;
+		this.archiveMarker = startPosition;
+		backupPosition = new Vector2(startPosition.x, startPosition.y);
+		position = startPosition;
+		facingDirection = Direction.NORTH;
+		recentlyBackuped = true;
 
-        setOrigin(TILE_SCALE/2, TILE_SCALE/2);
-        
-        setXPositionOnBoard();
-        setYPositionOnBoard();
-    }
+		// All regarding spritesheet and getting the frames correctly is done here.
+		spriteSheet = new Texture(textureSpriteSheet);
+		TextureRegion[][] tmp = TextureRegion.split(spriteSheet, spriteSheet.getWidth() / FRAME_COL,
+				spriteSheet.getHeight() / FRAME_ROW);
+		animationFrames = new TextureRegion[(FRAME_ROW * FRAME_COL) - 5];
+		int index = 0;
+		for (int i = 0; i < FRAME_ROW; i++) {
+			for (int j = 0; j < FRAME_COL; j++) {
+				if (i == 1 && j >= 3) {
+					continue;
+				}
+				animationFrames[index++] = tmp[i][j];
+			}
+		}
 
-    public String getName() {
-        return playerName;
-    }
+		robotAnimation = new Animation<TextureRegion>(ANIMATION_SPEED, animationFrames);
 
-    // Methods regarding animations
-    public Animation<TextureRegion> getRobotAnimation() {
-        return robotAnimation;
-    }
+		setOrigin(TILE_SCALE / 2, TILE_SCALE / 2);
 
-    // Methods regarding X and Y positions
-    private void animateXPositionOnBoard(float delta) {
-        setX(getX() + movementVelocity.x * delta);
-    }
-    private void animateYPositionOnBoard(float delta) {
-        setY(getY() + movementVelocity.y * delta);
-    }
-    public Vector2 getVector2Position() {
-        return position;
-    }
-    public int getXPosition() {
-        return (int) position.x;
-    }
-    public int getYPosition() {
-        return (int) position.y;
-    }
-    private void setXPositionOnBoard() {
-        setX(position.x * TILE_SCALE);
-    }
-    private void setYPositionOnBoard() {
-        setY(position.y * TILE_SCALE);
-    }
+		setXPositionOnBoard();
+		setYPositionOnBoard();
+	}
 
-    // Methods regarding direction
-    public Direction getFacingDirection() {
-        return facingDirection;
-    }
+	public String getName() {
+		return playerName;
+	}
 
-    public void moveInFacingDirection(){
-        moveDirection(facingDirection);
-    }
+	// Methods regarding animations
+	public Animation<TextureRegion> getRobotAnimation() {
+		return robotAnimation;
+	}
 
-    public void moveDirection(Direction dir) {
-        switch(dir) {
-            case EAST:
-                moveEast();
-                break;
-            case NORTH:
-                moveNorth();
-                break;
-            case SOUTH:
-                moveSouth();
-                break;
-            case WEST:
-                moveWest();
-                break;
-            default:
-                break;
-        }
-    }
-    private void moveEast() {
-        movingEast = true;
-        position.x += 1;
-    }
-    private void moveNorth() {
-        movingNorth = true;
-        position.y += 1;
-    }
-    private void moveSouth() {
-        movingSouth = true;
-        position.y -= 1;
-    }
-    private void moveWest() {
-        movingWest = true;
-        position.x -= 1;
-    }
-    // Rotates player 90 degrees clockwise for each numberOfTimes. 90 degrees counterclockwise when numberOfTimes is negative.
-    public void rotatePlayer(int numberOfTimes) {
-        int directionSum = (((facingDirection.ordinal() + numberOfTimes) % 4) + 4) % 4;
-        facingDirection = Direction.values()[directionSum];
+	// Methods regarding X and Y positions
+	private void animateXPositionOnBoard(float delta) {
+		setX(getX() + movementVelocity.x * delta);
+	}
 
-        targetRotation = (int) (this.getRotation() - (90 * numberOfTimes));
-        if(numberOfTimes < 0) {
-            rotatingRight = true;
-        } else {
-            rotatingLeft = true;
-        }
-    }
+	private void animateYPositionOnBoard(float delta) {
+		setY(getY() + movementVelocity.y * delta);
+	}
 
-    public void update(float delta) {
-        animateXPositionOnBoard(delta);
-        animateYPositionOnBoard(delta);
+	public Vector2 getVector2Position() {
+		return position;
+	}
 
-        if(movingNorth) {
-            // position.y * tileScale : translation of player's position to correct number of pixels
-            if(getY() >= position.y * TILE_SCALE) {
-                movingNorth = false;
-                movementVelocity.y = 0;
-                setYPositionOnBoard();       // "hard sets" the position to avoid inaccuracies in position
-            } else {
-                movementVelocity.y = MOVEMENT_SPEED;
-            }
-        } else if (movingSouth) {
-            if(getY() <= position.y  * TILE_SCALE) {
-                movingSouth = false;
-                setYPositionOnBoard();
-                movementVelocity.y = 0;
-            } else {
-                movementVelocity.y = -MOVEMENT_SPEED;
-            }
-        } else {
-            movementVelocity.y = 0;
-        }
+	public int getXPosition() {
+		return (int) position.x;
+	}
 
-        if(movingEast) {
-            if(getX() >= position.x * TILE_SCALE) {
-                movingEast = false;
-                setXPositionOnBoard();
-                movementVelocity.x = 0;
-            } else {
-                movementVelocity.x = MOVEMENT_SPEED;
-            }
-        } else if (movingWest) {
-            if(getX() <= position.x * TILE_SCALE) {
-                movingWest = false;
-                setXPositionOnBoard();
-                movementVelocity.x = 0;
-            } else {
-                movementVelocity.x = -MOVEMENT_SPEED;
-            }
-        } else {
-            movementVelocity.x = 0;
-        }
+	public int getYPosition() {
+		return (int) position.y;
+	}
 
-        if(rotatingLeft) {
-            if(this.getRotation() > targetRotation) {
-                this.rotate(-ROTATE_SPEED * delta);
-            } else {
-                this.setRotation(targetRotation);
-                rotatingLeft = false;
-            }
-        } else if(rotatingRight) {
-            if(this.getRotation() < targetRotation) {
-                this.rotate(ROTATE_SPEED * delta);
-            } else {
-                this.setRotation(targetRotation);
-                rotatingRight = false;
-            }
-        }
-    }
-    
-    public int numberOfCheckpointsPassed() {
-        return numberOfCheckpointsPassed;
-    }
-    
-    public void passCheckpoint() {
-        numberOfCheckpointsPassed++;
-        Vector2 checkpoint = this.getVector2Position();
-        setBackupPosition(checkpoint);
-    }
-    
-    public Vector2 getBackupPosition() {
-        return new Vector2(backupPosition.x, backupPosition.y);
-    }
-    
-    public void setBackupPosition(Vector2 lastCheckpoint) {
-        if(numberOfCheckpointsPassed() < 1) {
-            return; 
-        } else {
-            backupPosition = new Vector2(lastCheckpoint.x, lastCheckpoint.y);
-        }
-    }
+	private void setXPositionOnBoard() {
+		setX(position.x * TILE_SCALE);
+	}
 
-    public boolean getRecentlyBackuped() {
-        return recentlyBackuped;
-    }
-    public void setRecentlyBackuped(boolean bool) {
-        this.recentlyBackuped = bool;
-    }
-    
-    public void moveToLastCheckpoint() {
-    	position = getBackupPosition();
-    	setXPositionOnBoard();
-    	setYPositionOnBoard();
-    } 
-    
-    public Vector2 getArchiveMarker() {
-    	return new Vector2(archiveMarker.x, archiveMarker.y);
-    }
-    
-    public void moveToArchiveMarker() {
-    	position = getArchiveMarker();
-    	setXPositionOnBoard();
-    	setYPositionOnBoard();
-    }
-    
-    public int getDamageToken() {
-    	return this.damageToken;
-    }
-    
-    public void setDamageToken(int damage) {
-    	this.damageToken = damage;
-    }
-    
-    public int getHealth() {
-    	return this.health;
-    }
-    
-    public void setHealth(int health) {
-    	this.health = health;
-    }
+	private void setYPositionOnBoard() {
+		setY(position.y * TILE_SCALE);
+	}
 
-    
+	// Methods regarding direction
+	public Direction getFacingDirection() {
+		return facingDirection;
+	}
+
+	public void moveInFacingDirection() {
+		moveDirection(facingDirection);
+	}
+
+	public void moveDirection(Direction dir) {
+		switch (dir) {
+		case EAST:
+			moveEast();
+			break;
+		case NORTH:
+			moveNorth();
+			break;
+		case SOUTH:
+			moveSouth();
+			break;
+		case WEST:
+			moveWest();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void moveEast() {
+		movingEast = true;
+		position.x += 1;
+	}
+
+	private void moveNorth() {
+		movingNorth = true;
+		position.y += 1;
+	}
+
+	private void moveSouth() {
+		movingSouth = true;
+		position.y -= 1;
+	}
+
+	private void moveWest() {
+		movingWest = true;
+		position.x -= 1;
+	}
+
+	// Rotates player 90 degrees clockwise for each numberOfTimes. 90 degrees
+	// counterclockwise when numberOfTimes is negative.
+	public void rotatePlayer(int numberOfTimes) {
+		int directionSum = (((facingDirection.ordinal() + numberOfTimes) % 4) + 4) % 4;
+		facingDirection = Direction.values()[directionSum];
+
+		targetRotation = (int) (this.getRotation() - (90 * numberOfTimes));
+		if (numberOfTimes < 0) {
+			rotatingRight = true;
+		} else {
+			rotatingLeft = true;
+		}
+	}
+
+	public void update(float delta) {
+		animateXPositionOnBoard(delta);
+		animateYPositionOnBoard(delta);
+
+		if (movingNorth) {
+			// position.y * tileScale : translation of player's position to correct number
+			// of pixels
+			if (getY() >= position.y * TILE_SCALE) {
+				movingNorth = false;
+				movementVelocity.y = 0;
+				setYPositionOnBoard(); // "hard sets" the position to avoid inaccuracies in position
+			} else {
+				movementVelocity.y = MOVEMENT_SPEED;
+			}
+		} else if (movingSouth) {
+			if (getY() <= position.y * TILE_SCALE) {
+				movingSouth = false;
+				setYPositionOnBoard();
+				movementVelocity.y = 0;
+			} else {
+				movementVelocity.y = -MOVEMENT_SPEED;
+			}
+		} else {
+			movementVelocity.y = 0;
+		}
+
+		if (movingEast) {
+			if (getX() >= position.x * TILE_SCALE) {
+				movingEast = false;
+				setXPositionOnBoard();
+				movementVelocity.x = 0;
+			} else {
+				movementVelocity.x = MOVEMENT_SPEED;
+			}
+		} else if (movingWest) {
+			if (getX() <= position.x * TILE_SCALE) {
+				movingWest = false;
+				setXPositionOnBoard();
+				movementVelocity.x = 0;
+			} else {
+				movementVelocity.x = -MOVEMENT_SPEED;
+			}
+		} else {
+			movementVelocity.x = 0;
+		}
+
+		if (rotatingLeft) {
+			if (this.getRotation() > targetRotation) {
+				this.rotate(-ROTATE_SPEED * delta);
+			} else {
+				this.setRotation(targetRotation);
+				rotatingLeft = false;
+			}
+		} else if (rotatingRight) {
+			if (this.getRotation() < targetRotation) {
+				this.rotate(ROTATE_SPEED * delta);
+			} else {
+				this.setRotation(targetRotation);
+				rotatingRight = false;
+			}
+		}
+	}
+
+	public int numberOfCheckpointsPassed() {
+		return numberOfCheckpointsPassed;
+	}
+
+	public void passCheckpoint() {
+		numberOfCheckpointsPassed++;
+		Vector2 checkpoint = this.getVector2Position();
+		setBackupPosition(checkpoint);
+	}
+
+	public Vector2 getBackupPosition() {
+		return new Vector2(backupPosition.x, backupPosition.y);
+	}
+
+	public void setBackupPosition(Vector2 lastCheckpoint) {
+		if (numberOfCheckpointsPassed() < 1) {
+			return;
+		} else {
+			backupPosition = new Vector2(lastCheckpoint.x, lastCheckpoint.y);
+		}
+	}
+
+	public boolean getRecentlyBackuped() {
+		return recentlyBackuped;
+	}
+
+	public void setRecentlyBackuped(boolean bool) {
+		this.recentlyBackuped = bool;
+	}
+
+	public void moveToLastCheckpoint() {
+		position = getBackupPosition();
+		setXPositionOnBoard();
+		setYPositionOnBoard();
+	}
+
+	public Vector2 getArchiveMarker() {
+		return new Vector2(archiveMarker.x, archiveMarker.y);
+	}
+
+	public void moveToArchiveMarker() {
+		position = getArchiveMarker();
+		setXPositionOnBoard();
+		setYPositionOnBoard();
+	}
+
+	public int getDamageToken() {
+		return this.damageToken;
+	}
+
+	public void setDamageToken(int damage) {
+		this.damageToken = damage;
+	}
+
+	public int getHealth() {
+		return this.health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
 	public boolean damageTokenFull() {
 		if (damageToken == 10) {
 			return true;
@@ -292,34 +306,41 @@ public class PlayerToken extends Sprite {
 	}
 
 	public void addDamageToken() {
-		damageToken++;	
-		if(damageTokenFull()) {
-			takeHealth(); 
-			setDamageToken(2);
-			moveToArchiveMarker();	
-		} 
+		if (!damageTokenFull()) {
+			damageToken++;
+		}
 	}
-	
+
 	public void removeDamageToken() {
-		if(damageToken == 0) {
+		if (damageToken == 0) {
 			return;
 		}
 		damageToken--;
 	}
-	
+
 	public void takeHealth() {
 		health--;
-		if(health < 1) {
+		if (health < 1) {
 			setDestroyed(true);
 		}
 	}
-	
+
 	public boolean checkIfDestroyed() {
 		return this.destroyed;
 	}
-	
+
 	public void setDestroyed(boolean destroyed) {
 		this.destroyed = destroyed;
 	}
-    
+
+	public void checkForDamageCleanUp() {
+		if (damageTokenFull()) {
+			takeHealth();
+			setDamageToken(2);
+			moveToArchiveMarker();
+			return;
+		}
+		return;
+	}
+
 }
